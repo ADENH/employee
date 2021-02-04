87a7131b1cf74db1cd2cc8bea6e4be18862e5c8b
adenurhidayat <adenurhidayat101294@gmail.com>
2021-02-04 10:44:26 +0700

    change to logger

--- src/main/java/com/mmi/mmi/aspect/EmployeeAspect.java
+++ src/main/java/com/mmi/mmi/aspect/EmployeeAspect.java
@@ -1,20 +1,27 @@
 package com.mmi.mmi.aspect;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
+import com.mmi.mmi.config.LoggingAspect;
 import com.mmi.mmi.model.Employee;
 
 @Aspect
 @Component
 public class EmployeeAspect {
+	
+	private static final Logger LOGGER = LogManager.getLogger(LoggingAspect.class);
+	
 	@AfterReturning(value="execution(* com.mmi.mmi.service.serviceimpl.EmployeeServiceImpl.*(..))",returning="employee")  
 	public void afterReturningAdvice(JoinPoint joinPoint, Employee employee)  
 	{  
-	System.out.println("After Returing method:"+joinPoint.getSignature());  
-	System.out.println(employee);  
+	LOGGER.info("After Returing method:"+joinPoint.getSignature());  
+//	System.out.println(employee);  
+	LOGGER.info(employee);
 	}
 
 }
