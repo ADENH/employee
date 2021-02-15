e3fcaa5acf7079716e2008a9230ef41cdac754dc
adenurhidayat <adenurhidayat101294@gmail.com>
2021-02-15 20:51:15 +0700

    add unit test for PositionServiceImpl class

--- src/test/java/com/mmi/mmi/service/PositionServiceTest.java
+++ src/test/java/com/mmi/mmi/service/PositionServiceTest.java
@@ -1,17 +1,22 @@
 package com.mmi.mmi.service;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.EntityNotFoundException;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 
+import com.mmi.mmi.dto.PositionDTO;
+import com.mmi.mmi.model.entity.Employee;
 import com.mmi.mmi.model.entity.Position;
+import com.mmi.mmi.model.entity.User;
 import com.mmi.mmi.repository.PositionRepository;
 import com.mmi.mmi.service.serviceimpl.PositionServiceImpl;
 
@@ -21,31 +26,35 @@ public class PositionServiceTest {
 	@Mock
 	private PositionRepository positionRepository;
 	
-	@InjectMocks
-	private PositionService positionService = new PositionServiceImpl();
+	private PositionServiceImpl positionServiceImpl;
 	
 	@BeforeEach
 	void setUp() {
-		Position position = new Position();
-		position.setCode("MNG");
-		position.setIsDelete(0);
-		position.setName("Manager");
-		Mockito.when(positionRepository.findByCode(position.getCode())).thenReturn(position);
+		positionServiceImpl = new PositionServiceImpl(positionRepository);
 	}
 	
 	@Test
-	@Disabled
 	void getPositionByCodeTest() {
-//		String code = "MNG";
-//		PositionDTO found = positionService.getPositionByCode(code);
-//		assertThat(found.getCode()).isEqualTo(code);
+		Employee employee = new Employee();
+		Set<Employee> dataEmployee = new HashSet<Employee>();
+		dataEmployee.add(employee);
+		
+		User user = new User();
+		Set<User> dataUser = new HashSet<User>();
+		dataUser.add(user);
+		
+		Mockito.when(positionRepository.findByCode("MNG")).thenReturn(new Position(1,"MNG","Manager",0,dataEmployee,dataUser));
+		PositionDTO found = positionServiceImpl.getPositionByCode("MNG");
+		
+		Assertions.assertNotNull(found);;
+		Assertions.assertEquals("MNG", found.getCode());
+		Assertions.assertEquals("Manager", found.getName());
 	}
 	
 	@Test
-	@Disabled
 	void getPositionByCodeNotFoundTest(){
-		Assertions.assertThrows(NotFoundException.class, () ->{
-			positionService.getPositionByCode("not found");
+		Assertions.assertThrows(EntityNotFoundException.class, () ->{
+			positionServiceImpl.getPositionByCode("not found");
 		});
 	}
 	
