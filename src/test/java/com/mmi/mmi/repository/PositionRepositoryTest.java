0286afc58fe8f0af3a54050b866960b8e81f171c
adenurhidayat <adenurhidayat101294@gmail.com>
2021-02-17 13:49:41 +0700

    add new unit test for PositionRepository

--- src/test/java/com/mmi/mmi/repository/PositionRepositoryTest.java
+++ src/test/java/com/mmi/mmi/repository/PositionRepositoryTest.java
@@ -32,27 +32,66 @@ public class PositionRepositoryTest {
 	}
 	
 	@Test
-	void whenSave_thenReturnPosition() {
+	void whenFindByCode_thenReturnEmptyPosition() {
 		Position position = new Position("MNG","Manager");
+		Position result = positionRepository.findByCode(position.getCode());
+		Assertions.assertNull(result);
+	}
+	
+	@Test
+	void whenSave_thenReturnPosition() {
+		Position position = new Position("MNG","Manager",0);
 		
 		Position result = positionRepository.save(position);
 		
 		Assertions.assertNotNull(result);
 		Assertions.assertNotNull(result.getId());
+		Assertions.assertEquals(position.getCode(), result.getCode());
+		Assertions.assertEquals(position.getName(), result.getName());
+		Assertions.assertEquals(position.getIsDelete(), result.getIsDelete());
 		
 	}
 	
 	@Test
 	void whenFindByIsDelete_thenReturnAllPositionWithIsDeleteValueZero() {
 		Position position = new Position("MNG","Manager",0);
+		Position position2 = new Position("PGRM","Programmer",1);
 		
 		positionRepository.save(position);
+		positionRepository.save(position2);
 		Pageable pageable = PageRequest.of(0, 5);
 		Page<Position> dataPage = positionRepository.findByIsDelete(pageable, 0);
 		
 		Assertions.assertNotNull(dataPage.getContent());
+		Assertions.assertEquals(1, dataPage.getContent().size());
 		Assertions.assertNotNull(dataPage.getContent().get(0).getId());
-		Assertions.assertEquals(position.getCode(), dataPage.getContent().get(0).getCode());
+		Assertions.assertEquals("MNG", dataPage.getContent().get(0).getCode());
+		
+		
+	}
+	
+	@Test
+	void whenFindByIsDelete_thenReturnAllPositionWithIsDeleteValueOne() {
+		Position position = new Position("MNG","Manager",0);
+		Position position2 = new Position("PGRM","Programmer",1);
+		
+		positionRepository.save(position);
+		positionRepository.save(position2);
+		Pageable pageable = PageRequest.of(0, 5);
+		Page<Position> dataPage = positionRepository.findByIsDelete(pageable, 1);
+		
+		Assertions.assertNotNull(dataPage.getContent());
+		Assertions.assertNotNull(dataPage.getContent().get(0).getId());
+		Assertions.assertEquals("PGRM", dataPage.getContent().get(0).getCode());
+		Assertions.assertEquals(1, dataPage.getContent().size());
+		
+	}
+	
+	@Test
+	void whenFindByIsDelete_thenReturnEmpty() {
+		Pageable pageable = PageRequest.of(0, 5);
+		Page<Position> dataPage = positionRepository.findByIsDelete(pageable, 0);
+		Assertions.assertEquals(true, dataPage.getContent().isEmpty());
 		
 	}
 }
