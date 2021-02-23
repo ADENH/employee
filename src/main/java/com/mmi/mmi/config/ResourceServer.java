d37f0edbd1b89f779ef264838ebd265c7136cf91
adenurhidayat <adenurhidayat101294@gmail.com>
2021-02-23 11:28:46 +0700

    enable h2 console

--- src/main/java/com/mmi/mmi/config/ResourceServer.java
+++ src/main/java/com/mmi/mmi/config/ResourceServer.java
@@ -30,5 +30,7 @@ public class ResourceServer extends ResourceServerConfigurerAdapter {
 //        .antMatchers("/**").authenticated()
         .and()
         .httpBasic();
+		
+		http.headers().frameOptions().disable();
 	}
 }
